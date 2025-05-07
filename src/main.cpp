5e32f16cfbe4cc20081caca36ffaea4bdca34dd7
Nick Florez <128880858+NicoFJ09@users.noreply.github.com>
2025-05-07 14:22:19 -0600

    Feat(Raylib): Instalation and base screen

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include "raylib.h"
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    const char *text = "Genetic Kingdom";
+    const int fontSize = 20;
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            // Calculate text width and position it in the center
+            int textWidth = MeasureText(text, fontSize);
+            int textX = (screenWidth - textWidth) / 2;
+            int textY = (screenHeight - fontSize) / 2;
+
+            DrawText(text, textX, textY, fontSize, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}

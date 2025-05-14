3c89b1f6bc5f4f299f1838ef409ea7687083c14f
Nick Florez <128880858+NicoFJ09@users.noreply.github.com>
2025-05-14 15:42:37 -0600

    Feat(Costs): Added prices to items in bottom panel

--- src/entities/towers/archerTower/ArcherTower.cpp
+++ src/entities/towers/archerTower/ArcherTower.cpp
@@ -8,7 +8,6 @@ ArcherTower::ArcherTower(Vector2 position, int level)
         level == 1 ? 10 : level == 2 ? 15 : 20,           // damage
         level == 1 ? 2  : level == 2 ? 3  : 4,            // speed
         level == 1 ? 64 : level == 2 ? 128 : 192,        // range in pixels
-        level == 1 ? 10 : level == 2 ? 5 : 3,           // attack regen
         level == 1 ? 50 : level == 2 ? 40 : 25          // special attack regen
     ){
     towerType = "Archer Tower";
@@ -20,21 +19,18 @@ void ArcherTower::UpdateStats() {
         damage = 10;
         speed = 2;
         range = 64;
-        attackRegenerationTime = 10;
         spAttackRegenerationTime = 50;
         texturePath = "../assets/textures/towers/towersInMap/ArcherLvl1.png";
     } else if (level == 2) {
         damage = 15;
         speed = 3;
         range = 128;
-        attackRegenerationTime = 5;
         spAttackRegenerationTime = 40;
         texturePath = "../assets/textures/towers/towersInMap/ArcherLvl2.png";
     } else if (level == 3) {
         damage = 20;
         speed = 4;
         range = 192;
-        attackRegenerationTime = 3;
         spAttackRegenerationTime = 25;
         texturePath = "../assets/textures/towers/towersInMap/ArcherLvl3.png";
     }
--- src/entities/towers/artilleryTower/ArtilleryTower.cpp
+++ src/entities/towers/artilleryTower/ArtilleryTower.cpp
@@ -8,7 +8,6 @@ ArtilleryTower::ArtilleryTower(Vector2 position, int level)
         level == 1 ? 20 : level == 2 ? 30 : 40,           // damage
         level == 1 ? 1  : level == 2 ? 2  : 3,            // speed
         level == 1 ? 50 : level == 2 ? 100 : 150,        // range in pixels
-        level == 1 ? 15 : level == 2 ? 10 : 5,           // attack regen
         level == 1 ? 70 : level == 2 ? 50 : 35          // special attack regen
     ){
     towerType = "Artillery Tower";
@@ -20,21 +19,18 @@ void ArtilleryTower::UpdateStats() {
         damage = 20;
         speed = 1;
         range = 50;
-        attackRegenerationTime = 15;
         spAttackRegenerationTime = 70;
         texturePath = "../assets/textures/towers/towersInMap/ArtilleryLvl1.png";
     } else if (level == 2) {
         damage = 30;
         speed = 2;
         range = 100;
-        attackRegenerationTime = 10;
         spAttackRegenerationTime = 50;
         texturePath = "../assets/textures/towers/towersInMap/ArtilleryLvl2.png";
     } else if (level == 3) {
         damage = 40;
         speed = 3;
         range = 150;
-        attackRegenerationTime = 5;
         spAttackRegenerationTime = 35;
         texturePath = "../assets/textures/towers/towersInMap/ArtilleryLvl3.png";
     }
